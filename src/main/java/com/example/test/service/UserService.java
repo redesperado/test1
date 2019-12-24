946b2b9057a353ff768bee1ea99197db08f84c97
Jqf <13164376990@163.com>
2019-12-24 10:22:35 +0800

    添加注释

--- src/main/java/com/example/test/service/UserService.java
+++ src/main/java/com/example/test/service/UserService.java
@@ -6,16 +6,22 @@ import java.util.List;
 
 public interface UserService {
 
+    //登录时信息核对
     UserBean loginIn(String name,String password);
 
+    //根据ID查询用户信息
     UserBean queryUserById(String id);
 
+    //插入新的用户
     int addUser(UserBean userBean);
 
+    //删除用户
     int dropUser(String id);
 
+    //修改用户
     int modifyUser(UserBean userBean);
 
+    //查询所有用户
     List<UserBean> queryAllUser();
 
 }
--- src/main/java/com/example/test/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/test/serviceImpl/UserServiceImpl.java
@@ -15,35 +15,67 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
+    /**
+     * 登录验证
+     * @param name
+     * @param password
+     * @return
+     */
     @Override
     public UserBean loginIn(String name, String password) {
         return userMapper.getInfo(name,password);
     }
 
+    /**
+     * 根据ID查询用户信息
+     * @param id
+     * @return
+     */
     @Override
     public UserBean queryUserById(String id) {
         return userMapper.selectUserById(id);
     }
 
 
+    /**
+     * 新增用户
+     * @param userBean
+     * @return
+     */
     @Override
     public int addUser(UserBean userBean) {
         int aFlag = userMapper.insertUser(userBean);
         return aFlag;
     }
 
+    /**
+     * 根据ID删除用户
+     * @param id
+     * @return
+     */
     @Override
     public int dropUser(String id) {
         int dFlag = userMapper.deleteUser(id);
         return dFlag;
     }
 
+
+    /**
+     * 修改用户信息
+     * remark：实际上还是根据用户ID修改用户信息
+     * @param userBean
+     * @return
+     */
     @Override
     public int modifyUser(UserBean userBean) {
         int mFlag = userMapper.updateUser(userBean);
         return mFlag;
     }
 
+    /**
+     * 查询所有用户信息
+     * @return
+     */
     @Override
     public List<UserBean> queryAllUser() {
         return userMapper.getAllUser();
